2bd3fe5061f62b6ed189bbe6c9f83d3d2209fcad
Wellington Perez <wellingtonaperez@gmail.com>
2017-06-30 21:28:10 -0300

    Created request users test

---
+++ spec/requests/api/v1/users_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Users API', type: :request do
+  let!(:user) { create(:user) }
+  let(:user_id) { user.id }
+
+  describe "GET /users:id" do
+
+    before do
+      headers = { "Accept" => "application/vnd.taskmanager.v1" }
+      get "/users/#{user_id}", {}, headers
+    end
+
+    context "when the users exists" do
+      it "returns the user" do
+        user_response = JSON.parse(response.body)
+        expect(user_response["id"]).to eq(user_id)
+      end
+
+    end
+
+  end
+
+
+end
