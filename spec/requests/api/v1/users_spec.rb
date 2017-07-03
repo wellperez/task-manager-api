bdae5b36fc86b18c7fd7d8b2cdd3798436c54420
Wellington Perez <wellingtonaperez@gmail.com>
2017-07-03 10:51:35 -0300

    Added show action to the users controller

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -4,17 +4,31 @@ RSpec.describe 'Users API', type: :request do
   let!(:user) { create(:user) }
   let(:user_id) { user.id }
 
+  before { host! "api.qualquerdominio.dev" }
+
   describe "GET /users:id" do
 
     before do
       headers = { "Accept" => "application/vnd.taskmanager.v1" }
-      get "/users/#{user_id}", {}, headers
+      get "/users/#{user_id}", params: {}, headers: headers
     end
 
     context "when the users exists" do
       it "returns the user" do
         user_response = JSON.parse(response.body)
         expect(user_response["id"]).to eq(user_id)
+      end
+
+      it "returns status 200" do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "when the users does not exist" do
+      let(:user_id) { 0 }
+
+      it "returns status code 404" do
+        expect(response).to have_http_status(404)
       end
 
     end
