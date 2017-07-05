1d539da82d8bfac80cf11c43d6abf6bc90db936b
Wellington Perez <wellingtonaperez@gmail.com>
2017-07-05 12:02:36 -0300

    Symbolizing response body keys

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -14,8 +14,8 @@ RSpec.describe 'Users API', type: :request do
 
     context 'when the users exists' do
       it 'returns the user' do
-        user_response = JSON.parse(response.body)
-        expect(user_response['id']).to eq(user_id)
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:id]).to eq(user_id)
       end
 
       it 'returns status 200' do
@@ -45,8 +45,8 @@ RSpec.describe 'Users API', type: :request do
       end
 
       it 'return json data' do
-        user_response = JSON.parse(response.body)
-        expect(user_response['email']).to eq(user_params[:email])
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eq(user_params[:email])
       end
     end
 
@@ -58,8 +58,8 @@ RSpec.describe 'Users API', type: :request do
       end
 
       it 'return the json data for the errors' do
-        user_response = JSON.parse(response.body)
-        expect(user_response).to have_key('errors')
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
       end
     end
   end
