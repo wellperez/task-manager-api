54c851d055e475941d8cb4deb8525818d47fb28b
Wellington Perez <wellingtonaperez@gmail.com>
2017-07-11 16:58:08 -0300

    Adding user authentication token

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_uniqueness_of :auth_token
+  before_create :generate_authentication_token!
 
   def info
     "#{email} - #{created_at} - Token: #{Devise.friendly_token}"
+  end
+
+  def generate_authentication_token!
+    loop do
+      self.auth_token = Devise.friendly_token
+      break unless User.exists?(auth_token: auth_token)
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,8 +14,24 @@ RSpec.describe User, type: :model do
       user.save!
       allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
 
-      expect(user.info).to eq("#{user.email} - #{user.created_at}
-       - Token: #{Devise.friendly_token}")
+      expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: #{Devise.friendly_token}")
+    end
+  end
+
+  describe '#generate_authentication_token!' do
+    it 'generates a unique auth token' do
+      allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
+      user.generate_authentication_token!
+
+      expect(user.auth_token).to eq('abc123xyzTOKEN')
+    end
+
+    it 'generates another auth token when the current auth token already has been taken' do
+      allow(Devise).to receive(:friendly_token).and_return('abc123tokenxyz', 'abc123tokenxyz', 'abcxyz123456789')
+      existing_user = create(:user, auth_token: 'abc123tokenxyz')
+      user.generate_authentication_token!
+
+      expect(user.auth_token).not_to eq(existing_user.auth_token)
     end
   end
 
