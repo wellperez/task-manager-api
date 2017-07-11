20b78f8a63a863893f34a6064d6b56cdb8e26eae
Wellington Perez <wellingtonaperez@gmail.com>
2017-07-11 14:59:40 -0300

    Adding auth token and #info to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { build(:user)}
+  let(:user) { build(:user) }
 
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   it { is_expected.to validate_confirmation_of(:password) }
-  it { is_expected.to allow_value("wellingtonaperez@gmail.com").for(:email) }
+  it { is_expected.to allow_value('wellingtonaperez@gmail.com').for(:email) }
+  it { is_expected.to validate_uniqueness_of(:auth_token) }
+
+  describe '#info' do
+    it 'returns email and created_at' do
+      user.save!
+
+      expect(user.info).to eq("#{user.email} - #{user.created_at}")
+    end
+  end
 
   # it { expect(user).to respond_to(:email) }
 
