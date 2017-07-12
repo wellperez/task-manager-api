3a53ad4af1e5f15db0a46e6e20b9aca326c11ff2
Wellington Perez <wellingtonaperez@gmail.com>
2017-07-12 14:19:00 -0300

    Adding create action to the sessions controller with tests

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,24 @@
+# sessions controllers
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: session_params[:email])
+
+    return render json: { errors: 'Invalid password or email' }, status: 401 unless user && user.valid_password?(
+      session_params[:password]
+    )
+    sign_in user, store: false
+    user.generate_authentication_token!
+    user.save!
+    render json: user, status: 200
+
+    # if user && user.valid_password?(session_params[:password])
+    #   render json: user, status: 200
+    # end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+end
