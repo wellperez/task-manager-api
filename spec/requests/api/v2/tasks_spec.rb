4b4e9bf53e0ec95c327a0a3ca761663e57c6e1c0
Wellington Perez <wellingtonaperez@gmail.com>
2017-07-25 14:38:15 -0300

    Fixing task tests for api v2

--- spec/requests/api/v2/tasks_spec.rb
+++ spec/requests/api/v2/tasks_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe 'Task API' do
     end
 
     it 'returns 5 tasks from database' do
-      expect(json_body[:tasks].count).to eq(5)
+      expect(json_body[:data].count).to eq(5)
     end
   end
 
@@ -35,7 +35,7 @@ RSpec.describe 'Task API' do
 
     context 'when the tasks exists' do
       it 'returns the task' do
-        expect(json_body[:title]).to eq(task.title)
+        expect(json_body[:data][:attributes][:title]).to eq(task.title)
       end
 
       it 'returns status 200' do
@@ -60,11 +60,11 @@ RSpec.describe 'Task API' do
       end
 
       it 'returns json data' do
-        expect(json_body[:title]).to eq(task_params[:title])
+        expect(json_body[:data][:attributes][:title]).to eq(task_params[:title])
       end
 
       it 'assigns the created task to the current user' do
-        expect(json_body[:user_id]).to eq(user.id)
+        expect(json_body[:data][:attributes][:'user-id']).to eq(user.id)
       end
     end
 
@@ -99,7 +99,7 @@ RSpec.describe 'Task API' do
       end
 
       it 'returns the json for updated task' do
-        expect(json_body[:title]).to eq(task_params[:title])
+        expect(json_body[:data][:attributes][:title]).to eq(task_params[:title])
       end
 
       it 'updates the task in the database' do
